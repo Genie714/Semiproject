d3fdd0876683fd628083e52d092fd2ec6a97b1d6
RKDALSGH0704 <hogus0704@naver.com>
2024-05-03 16:06:39 +0900

    클래스 5개 수정본
    maintest, alcoholvendingmachine, singleitem, presetcocktail, customcocktail

---
+++ src/AlcoholVendingMachine.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+abstract class AlcoholVendingMachine					// 상위클래스
+{
+	
+
+	protected abstract void alcoholReturn(int al);		// 술을 반환하는 메소드
+	
+	protected abstract void list();						// 술 리스트
+		
+	protected abstract int change(int al);				// 고른 술에 대한 값을 입력받은 값에서 빼주는 메소드
+	
+	protected int alcoholChoose()						// 술을 선택하는 메소드
+	{
+		
+		int al;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("술을 선택하시오 : ");
+		al = sc.nextInt();
+		return al;
+	}
+	
+	protected int getMoney()							// 돈 입력받는 메소드
+	{
+		PayMoney pm = PayMoney.getInstance();
+		pm.inputMoney(money);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("돈을 입력하시오 : ");
+		int money = sc.nextInt();
+
+		return money;
+
+
+	}
+
+}
--- src/MainTest.java
+++ src/MainTest.java
@@ -6,28 +6,71 @@
  * 당첨금 반환
  * 객체 내보내기 */
 
+import java.util.Scanner;
+
 public class MainTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		// 객체 불러오기 or 객체 생성하기
-		LoadObject lo = new LoadObject();
+		// 객체 불러오기 → 미구현 임시 클래스 인스턴스 생성
+//		LoadObject lo = new LoadObject();
+//		lo.loadObject();
 		
-		lo.loadObject();
 		
 		//자판기 안내 및 잭팟 예정금 디스플레이
+		PrizeMoneyDisplay pmd = new PrizeMoneyDisplay();
+		print();
+		pmd.showTotalPrize();
 		
 		
-		//Alcohol 클래스 진행
+		//Alcohol 클래스 진행(Alcohol 복사 붙여넣기)
+		SingleItem ob1 = new SingleItem();
+		PresetCocktail ob2 = new PresetCocktail();
+		CustomCocktail ob3 = new CustomCocktail();
 		
-		//룰렛 클래스 진행
+		if(a==1)
+		{
+			ob1.alcoholRun();
+			// 잔돈 반환 //룰렛 클래스 진행
+			ChangeMoney cm = new ChangeMoney();
+			cm.returnMoney(ob1.change(al));
+		}
+		
+		if(a==2)
+		{	
+			ob2.alcoholRun();
+			// 잔돈 반환 //룰렛 클래스 진행
+			ChangeMoney cm = new ChangeMoney();
+			cm.returnMoney(ob2.change(al));
+		}
+		
+		if(a==3)
+		{
+			ob3.alcoholRun();
+			// 잔돈 반환 //룰렛 클래스 진행
+			ChangeMoney cm = new ChangeMoney();
+			cm.returnMoney(ob3.change(price));
+		}
 		
 		//당첨금 반환
 		
-		//객체 내보내기
-		SaveObject so = new SaveObject();
 		
-		so.saveObject();
 		
+		//객체 내보내기 → 미구현
+		//SaveObject so = new SaveObject();
+		
+		//so.saveObject(ad,buy,alc);
+		
+	}
+	
+	private static void print()
+	{
+		System.out.println("=====================================");
+		System.out.println(" ■■■ 500룰렛 칵테일 자판기 ■■■");
+		System.out.println("=====================================");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("3가지 유형 중 하나를 선택하세요 (① 단품 / ② 프리셋(칵테일) / ③ 커스텀(칵테일) : ");
+		int a = sc.nextInt();
 	}
 }
